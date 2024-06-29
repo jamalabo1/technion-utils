da1de55ec5201d5e3a805d5140f42a763e54dfd8
Jamal Abo Mokh <25992049+jamalabo1@users.noreply.github.com>
2024-06-29 19:43:42 +0300

    improve test runner & added generators

--- engine/runner2.py
+++ engine/runner2.py
@@ -1,8 +1,9 @@
 import subprocess
-import conftest
+
+from testcase import RunnerCase
 
 
-def test_target(target, command, exec, verify):
-    print("running")
-    subprocess.run(exec)
-    subprocess.run(verify)
+def test_target(target, test_case: RunnerCase):
+    for command in test_case.commands:
+        command_result = command.runner()
+        assert command_result
