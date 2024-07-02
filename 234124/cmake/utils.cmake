bf68220a623e1b98989b0ae231349f722712a6dc
Jamal Abo <12488208-jamalabo1@users.noreply.gitlab.com>
2024-07-02 17:22:26 +0000

    change required files

--- 234124/cmake/utils.cmake
+++
@@ -1,6 +0,0 @@
-macro(install_project_binaries target)
-    install(TARGETS ${target}
-            DESTINATION bin
-            COMPONENT ${target}
-    )
-endmacro()
--- 234124/tests/engine/runner2.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-from testcase import RunnerCase
-
-
-def test_target(target, test_case: RunnerCase):
-    for command in test_case.commands:
-        command_result = command.runner()
-        assert command_result
